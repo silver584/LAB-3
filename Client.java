bc78e5a010b432cd4ce62b82e5e7d7b0e79afacf
silver584 <2649968259@qq.com>
2025-05-08 20:22:01 +0800

    Add files via upload

--- Client.java
+++ Client.java
@@ -13,8 +13,25 @@ public class Client {
 
 
 
-        
+
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            try (BufferedReader fileReader = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = fileReader.readLine()) != null) {
+                    String request = buildRequest(line);
+                    if (request == null) {
+                        System.err.println("Invalid line: " + line);
+                        continue;
+                    }
+                    out.println(request);
+                    String response = in.readLine();
+                    System.out.println(line + ": " + response);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
-}
