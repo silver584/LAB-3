7e18b18f01b4968bdf0358527944d5cc4a58c257
silver584 <2649968259@qq.com>
2025-05-08 20:17:18 +0800

    Add files via upload

--- Client.java
+++ Client.java
@@ -3,6 +3,18 @@ import java.io.*;
 import java.net.*;
 import java.util.Arrays;
 public class Client {
+    public static void main(String[] args) {
+
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        String filePath = args[2];
+
+
+
+
+        
+    }
 
 
 }
